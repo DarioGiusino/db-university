2eff721d155829fb939d15c540cbb060faf3bea0
DarioGiusino <dario.giusino@gmail.com>
2023-02-14 17:30:33 +0100

    group(3)

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -62,6 +62,8 @@ FROM `teachers`
 GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `exam_id` AS 'exam appeal', AVG(`vote`) AS 'average vote'
+FROM `exam_student`
+GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
