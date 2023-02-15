80abceabb10f568cddaa2c90d93cb339fd890a04
DarioGiusino <dario.giusino@gmail.com>
2023-02-15 17:30:33 +0100

    join(5)

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -112,7 +112,15 @@ ON `departments`.`id` = `degrees`.`department_id`
 ORDER BY `students`.`surname` ASC;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-
+SELECT `degrees`.`name` AS 'degree name', `courses`.`name` AS 'course name', `teachers`.`name` AS 'teacher name', `teachers`.`surname` AS 'teacher surname'
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+ORDER BY `degrees`.`name` ASC;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
